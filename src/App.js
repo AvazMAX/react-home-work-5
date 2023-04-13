247469e974707634afc477290ab2a63ae2b62d28
Avaz <almazavas@gmail.com>
2023-04-13 17:24:42 +0600

    save

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+import { Header } from "./Header/Header";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [open, setOpen] = useState(false);
+  // const [inputValue, setInputValue] = useState(
+  //   localStorage.getItem("inputValue") || ""
+  // );
+
+  const addTodoHandler = (data) => {
+    setTodos([...todos, data]);
+  };
+
+  const click = () => {
+    setOpen(true);
+  };
+  return (
+    <div className="App">
+      <Header
+        addTodoHandler={addTodoHandler}
+        todos={todos}
+        opoenModalHandler={click}
+        open={open}
+        setOpen={setOpen}
+        setTodos={setTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
